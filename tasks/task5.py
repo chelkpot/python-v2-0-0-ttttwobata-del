379c56b63e7bb9687d65e1802919f3c19536ea30
ttttwobata-del <ttttwobata@gmail.com>
2025-10-22 00:03:17 +0500

    Update task5.py

--- tasks/task5.py
+++ tasks/task5.py
@@ -1,10 +1,14 @@
-# tasks/task5.py
+# tasks/task4.py
 
 def solve():
 # Ниже пишите решение задачи
+    a = input().strip()
+    b = input().strip()
+    c = input().strip()
+    print(f"{a}---{b}---{c}")
 
+    
 
-   
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
